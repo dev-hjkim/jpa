bbf99a4057dfba0df7769d48fab2606fe540ad0c
dev-hjkim <dev--test@naver.com>
2022-01-02 01:12:40 +0900

    220102, jpa use repository interface

---
+++ src/main/java/com/example/jpa/CommentRepository.java
@@ -0,0 +1,12 @@
+package com.example.jpa;
+
+import org.springframework.data.repository.RepositoryDefinition;
+
+import java.util.List;
+
+public interface CommentRepository extends MyRepository<Comment, Long>{
+
+//    Comment save(Comment comment);
+//
+//    List<Comment> findAll();
+}
---
+++ src/test/java/com/example/jpa/CommentRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.example.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class CommentRepositoryTest {
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    @Test
+    public void crud() {
+        Comment comment = new Comment();
+        comment.setCommnet("Hello Comment");
+        commentRepository.save(comment);
+
+        List<Comment> all = commentRepository.findAll();
+        assertThat(all.size()).isEqualTo(1);
+
+        long count = commentRepository.count();
+        assertThat(count).isEqualTo(1);
+    }
+
+}
