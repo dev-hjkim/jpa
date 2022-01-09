d8fb3b250642e648db7bb4f684fb7a3f437fb467
dev-hjkim <dev--test@naver.com>
2022-01-10 00:10:21 +0900

    220110, jpa pageable & hateoas

--- src/main/java/com/example/jpa/post3/Post3Controller.java
+++ src/main/java/com/example/jpa/post3/Post3Controller.java
@@ -1,6 +1,12 @@
 package com.example.jpa.post3;
 
+import com.example.jpa.post2.Post2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +22,15 @@ public class Post3Controller {
     @GetMapping("/posts3/{id}")
     public String getPost(@PathVariable("id") Post3 post) {
         return post.getTitle();
+    }
+
+    @GetMapping("/posts3")
+    public Page<Post3> getPosts(Pageable pageable) {
+        return postRepository3.findAll(pageable);
+    }
+
+    @GetMapping("/posts3/hateoas")
+    public PagedModel<EntityModel<Post3>> getPosts(Pageable pageable, PagedResourcesAssembler<Post3> assembler) {
+        return assembler.toModel(postRepository3.findAll(pageable));
     }
 }
--- src/test/java/com/example/jpa/post3/Post3ControllerTest.java
+++ src/test/java/com/example/jpa/post3/Post3ControllerTest.java
@@ -8,12 +8,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -37,6 +38,46 @@ public class Post3ControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string("jpa"));
+    }
+
+    @Test
+    public void getPosts() throws Exception {
+        Post3 post = new Post3();
+        post.setTitle("jpa");
+        postRepository.save(post);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/posts3/")
+                        .param("page", "0")
+                        .param("size", "10")
+                        .param("sort", "created,desc")
+                        .param("sort", "title"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].id").value("1001"));
+    }
+
+    @Test
+    public void getPostsHateoas() throws Exception {
+        createPosts3();
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/posts3/hateoas/")
+                        .param("page", "3")
+                        .param("size", "10")
+                        .param("sort", "created,desc")
+                        .param("sort", "title"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._embedded.post3List[0].title").value("jpa"));
+    }
+
+    private void createPosts3() {
+        int postsCount = 100;
+        while (postsCount > 0) {
+            Post3 post = new Post3();
+            post.setTitle("jpa");
+            postRepository.save(post);
+            postsCount--;
+        }
     }
 
 
