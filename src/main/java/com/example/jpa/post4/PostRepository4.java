0004b6397defebd5e9f596acf60bf5e9a10c5059
dev-hjkim <dev--test@naver.com>
2022-01-13 21:15:55 +0900

    220113, jpa repository, SpEL

--- src/main/java/com/example/jpa/post4/PostRepository4.java
+++ src/main/java/com/example/jpa/post4/PostRepository4.java
@@ -3,6 +3,7 @@ package com.example.jpa.post4;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,6 +11,6 @@ public interface PostRepository4 extends JpaRepository<Post4, Long> {
 
     List<Post4> findByTitleStartsWith(String title);
 
-    @Query("SELECT p FROM Post4 AS p WHERE p.title = ?1")
-    List<Post4> findByTitle(String title, Sort sort);
+    @Query("SELECT p FROM #{#entityName} AS p WHERE p.title = :title")
+    List<Post4> findByTitle(@Param("title") String keyword, Sort sort);
 }
