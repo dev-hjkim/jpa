0d398f98dbd66cd3dbabeffa670dbd106928acf6
dev-hjkim <dev--test@naver.com>
2022-01-17 00:14:28 +0900

    220117, jpa repository, queryByExample

--- src/main/java/com/example/jpa/post4/CommentRepository4.java
+++ src/main/java/com/example/jpa/post4/CommentRepository4.java
@@ -3,10 +3,11 @@ package com.example.jpa.post4;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
 
 import java.util.List;
 
-public interface CommentRepository4 extends JpaRepository<Comment4, Long>, JpaSpecificationExecutor<Comment4> {
+public interface CommentRepository4 extends JpaRepository<Comment4, Long>, JpaSpecificationExecutor<Comment4>, QueryByExampleExecutor<Comment4> {
 
     @EntityGraph(attributePaths = {"post"})
     Comment4 getById(Long id);
--- src/test/java/com/example/jpa/post4/CommentRepository4Test.java
+++ src/test/java/com/example/jpa/post4/CommentRepository4Test.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -45,5 +47,18 @@ class CommentRepository4Test {
     @Test
     public void specs() {
         Page<Comment4> page = commentRepository.findAll(isBest().and(isGood()), PageRequest.of(0, 10));
+    }
+
+    @Test
+    public void qbe() {
+        Comment4 prove = new Comment4();
+        prove.setBest(true);
+
+        ExampleMatcher exampleMatcher = ExampleMatcher.matchingAny()
+                .withIgnorePaths("up", "down");
+
+        Example<Comment4> example = Example.of(prove, exampleMatcher);
+
+        commentRepository.findAll(example);
     }
 }
