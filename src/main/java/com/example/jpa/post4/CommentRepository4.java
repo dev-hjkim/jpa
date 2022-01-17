0d0ca04234282d270d98410d9a43d04594c56718
dev-hjkim <dev--test@naver.com>
2022-01-18 00:13:07 +0900

    220118, jpa repository, transaction

--- src/main/java/com/example/jpa/post4/CommentRepository4.java
+++ src/main/java/com/example/jpa/post4/CommentRepository4.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,5 +13,6 @@ public interface CommentRepository4 extends JpaRepository<Comment4, Long>, JpaSp
     @EntityGraph(attributePaths = {"post"})
     Comment4 getById(Long id);
 
+    @Transactional(readOnly = true) // flush를 NEVER로 설정하여 dirty checking을 막음 -> 성능 향상
     <T> List<T> findByPost_Id(Long id, Class<T> type);
 }
